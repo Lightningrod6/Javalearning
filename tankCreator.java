f3feb371a984225f5d452d59176873efccb6f01b
Lightning <48145941+Lightningrod6@users.noreply.github.com>
2023-11-29 05:31:42 +0000

    Added stuff lmao

--- tankCreator.java
+++ tankCreator.java
@@ -3,26 +3,85 @@ import java.util.Scanner;
 public class tankCreator {
 
     public enum TankType {
-        LIGHT, MEDIUM, HEAVY
-    }
-    public enum ArmorType {
-        EXPLOSIVE_REACTIVE,
-        NON_EXPLOSIVE_REACTIVE,
-        COMPOSITE,
-        STEEL
-    }
-    
-    public enum ShellType {
-        ARMOR_PIERCING,
-        HIGH_EXPLOSIVE,
-        HEAT,
-        SABOT
+        LIGHT("Light"), 
+        MEDIUM("Medium"), 
+        HEAVY("Heavy");
+
+        private final String displayName2;
+
+        TankType(String displayName2) {
+            this.displayName2 = displayName2;
+        }
+        
+        @Override
+        public String toString() {
+            return displayName2;
+        }
     }
 
+
+
+    public enum ArmorType {
+        EXPLOSIVE_REACTIVE("Explosive Reactive"),
+        NON_EXPLOSIVE_REACTIVE("Non-Explosive Reactive"),
+        COMPOSITE("Composite"),
+        STEEL("Steel");
+
+        private final String displayName3;
+
+        // Constructor to initialize the displayName
+        ArmorType(String displayName3) {
+            this.displayName3 = displayName3;
+        }
+
+        @Override
+        public String toString() {
+            return displayName3;
+        }
+    }        
+    
+    public enum Caliber {
+        NINETY_MM("90mm"),
+        ONE_HUNDRED_FIVE_MM("105mm"),
+        SEVENTY_SIX_MM("76mm"),
+        ONE_HUNDRED_MM("100mm"),
+        ONE_HUNDRED_TWENTY_MM("120mm");
+    
+        private final String displayName;
+    
+        // Constructor to initialize the displayName
+        Caliber(String displayName) {
+            this.displayName = displayName;
+        }
+    
+        // Override toString method
+        @Override
+        public String toString() {
+            return displayName;
+        }
+    }
+
+    public enum ShellType {
+        ARMOR_PIERCING("Armor Piercing"),
+        HIGH_EXPLOSIVE("High Explosive"),
+        HEAT("High Explosive Anti-Tank"),
+        SABOT("SABOT");
+
+        private final String displayName4;
+
+        ShellType(String displayName4) {
+            this.displayName4 = displayName4;
+        }
+
+        @Override
+        public String toString() {
+            return displayName4;
+        }
+    }
 
 
     private int speed, turningSpeed, shellVelocity, armorThickness;
-    private String shell;
+    private Caliber caliber;
     private String tankName;
     private TankType tankType;
     private ArmorType armorType;
@@ -30,22 +89,22 @@ public class tankCreator {
 
     public tankCreator() {
         speed = 16; // mph
-        turningSpeed = 20; // Degrees per second
+        turningSpeed = 30; // Degrees per second
         shellVelocity = 140; //meters per second
         armorThickness = 20; // millimeters
-        shell = "100mm"; 
+        caliber = Caliber.ONE_HUNDRED_MM; 
         tankType = TankType.LIGHT;
         tankName = "Base Tank";
         armorType = ArmorType.EXPLOSIVE_REACTIVE;
         shellType = ShellType.ARMOR_PIERCING; //AP or HE(High Explosive)
     }
 
-    public tankCreator(int spd, int tspd, int bullvelo, int armrthcknss, String bull, TankType tnktype, String tnkname, ArmorType armrtype, ShellType bullType) {
+    public tankCreator(int spd, int tspd, int bullvelo, int armrthcknss, Caliber bull, TankType tnktype, String tnkname, ArmorType armrtype, ShellType bullType) {
         speed = spd;
         turningSpeed = tspd;
         shellVelocity = bullvelo;
         armorThickness = armrthcknss;
-        shell = bull;
+        caliber = bull;
         tankType = tnktype;
         tankName = tnkname;
         armorType = armrtype;
@@ -68,8 +127,8 @@ public class tankCreator {
         return armorThickness;
     }
 
-    public String getshell() {
-        return shell;
+    public Caliber getCaliber() {
+        return caliber;
     }
 
     public TankType getTankType() {
@@ -107,12 +166,63 @@ public class tankCreator {
                 System.out.println("Invalid choice. Defaulting to LIGHT.");
                 setTankType(TankType.LIGHT);
         }
-        scanner.close();
         return this.tankType;
-
-        
     }
 
+    public Caliber caliberDecider() {
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Choose a Caliber {1: 76mm, 2:90mm, 3: 100mm, 4: 105mm, 5. 120mm }");
+        int choice = scanner2.nextInt();
+
+        switch (choice) {
+            case 1:
+                setCaliber(Caliber.SEVENTY_SIX_MM);
+            break;
+            case 2:
+                setCaliber(Caliber.NINETY_MM);
+            break;
+            case 3:
+                setCaliber(Caliber.ONE_HUNDRED_MM);
+            break;
+            case 4:
+                setCaliber(Caliber.ONE_HUNDRED_FIVE_MM);
+            break;
+            case 5: 
+                setCaliber(Caliber.ONE_HUNDRED_TWENTY_MM);
+            break;
+                default:
+                System.out.println("Invalid choice. Defaulting to 76mm.");
+                setCaliber(Caliber.SEVENTY_SIX_MM);         
+        }
+
+        return this.caliber;
+    }
+
+    public ArmorType armorTypeDecider() {
+        Scanner scanner3 = new Scanner(System.in);
+        System.out.println("Choose an armor type {1: Explosive Reactive (-4 max speed) 2: Non-Explosive Reactive (-2 max speed) 3: Composite (-8 Max speed) 4: Steel (-10)}");
+        int choice = scanner3.nextInt();
+
+        switch (choice) {
+            case 1:
+                setArmorType(ArmorType.EXPLOSIVE_REACTIVE);
+            break;
+            case 2:
+                setArmorType(ArmorType.NON_EXPLOSIVE_REACTIVE);
+            break;
+            case 3:
+                setArmorType(ArmorType.COMPOSITE);
+            break;
+            case 4:
+                setArmorType(ArmorType.STEEL);
+            break;
+            default:
+                System.out.println("Invalid Choice, Defaulting to Non-Explosive Reactive");
+                setArmorType(ArmorType.NON_EXPLOSIVE_REACTIVE);
+        }
+
+        return this.armorType;
+    }        
     public void tankPrintDefaultValues() {
         System.out.println("Tank Details");
         System.out.println("----------------------------------------");
@@ -122,7 +232,7 @@ public class tankCreator {
         System.out.println("Tank Turning Speed: " + turningSpeed + "aps(angle per second)");
         System.out.println("Tank Armor: " + armorType);
         System.out.println("Tank Armor Thickness: " + armorThickness + "mm");
-        System.out.println("Tank Shell Ammo: " + shell);
+        System.out.println("Tank Shell Ammo: " + caliber);
         System.out.println("Shell Velocity: " + shellVelocity + "");
         System.out.println("Shell Type: " + shellType);
     }
@@ -130,4 +240,25 @@ public class tankCreator {
 	public void setTankType(TankType newTankType) {
         this.tankType = newTankType;
 	}
+
+    public void setCaliber(Caliber newCaliber) {
+        this.caliber = newCaliber;
+    }
+
+    public void setTankSpeed(int newspeed) {
+        this.speed = newspeed;
+    }
+    public void setTurningSpeed(int newTurningSpeed) {
+        this.turningSpeed = newTurningSpeed;
+    }
+
+    public void setArmorType(ArmorType newArmor) {
+        this.armorType = newArmor;
+    }
+    
+    public void setArmorThickness(int newArmorThickness) {
+        this.armorThickness = newArmorThickness;
+    }
+
+    
 }
--- tankOne.java
+++ tankOne.java
@@ -1,12 +1,81 @@
+import java.util.Scanner;
 
 public class tankOne {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner keyboard = new Scanner(System.in);
         tankCreator javelin = new tankCreator();
         tankCreator.TankType decision = javelin.tankDecider();
         if (decision == tankCreator.TankType.LIGHT) {
-            System.out.println("Selected tank type: LIGHT");
+            System.out.println("Selected tank type: Light");
+            Thread.sleep(2000);
+            System.out.println("Generating Appropriate Values...");
+            Thread.sleep(5000);
+            System.out.println("Generated Values!");
+            Thread.sleep(1000);
+            System.out.println("Its time to edit the properties of the tank");
+            System.out.print("Enter the max speed you want for the tank: ");
+            int speedoption = keyboard.nextInt();
+            if (speedoption > 36) {
+                System.out.println("Cannot go above 36mph. Please enter a new speed:");
+                speedoption = keyboard.nextInt();
+            }
+            javelin.setTankSpeed(speedoption);
+            System.out.println("Great! The tank speed has been set to " + javelin.getTankSpeed());
+            Thread.sleep(1000);
+            System.out.println("Now its time to set the turning speed of the tank");
+            int turningSpeedOption = keyboard.nextInt();
+            if (turningSpeedOption > 30) {
+                System.out.println("Cannot go above 30a/s(Angle per second). Please enter a new speed:");
+                turningSpeedOption = keyboard.nextInt();
+            }
+            javelin.setTurningSpeed(turningSpeedOption);
+            System.out.println("The turning speed has now been set to " + javelin.getTankTurningSpeed());
+            Thread.sleep(1000);
+            System.out.println("Now its time to choose what Caliber you will use, High calibers mean more damage but more drop off if your shooting from far away");
+            tankCreator.Caliber decision2 = javelin.caliberDecider();
+            if (decision2 == tankCreator.Caliber.SEVENTY_SIX_MM) {
+                System.out.println("Selected Caliber: 76mm\n");
+                Thread.sleep(2000);
+            } else if(decision2 == tankCreator.Caliber.NINETY_MM) {
+                System.out.println("Selected Caliber: 90mm\n");
+                Thread.sleep(2000);
+            } else if(decision2 == tankCreator.Caliber.ONE_HUNDRED_MM) {
+                System.out.println("Selected Caliber: 100mm\n");
+                Thread.sleep(2000);
+            } else if(decision2 == tankCreator.Caliber.ONE_HUNDRED_FIVE_MM) {
+                System.out.println("Selected Caliber: 105mm\n");
+                Thread.sleep(2000);
+            } else if(decision2 == tankCreator.Caliber.ONE_HUNDRED_TWENTY_MM) {
+                System.out.println("Selected Caliber: 120mm\n");
+                Thread.sleep(2000);
+            }
+            javelin.setCaliber(decision2);
+            System.out.println("Now its time to choose the armor type. These can vary in how strong your tank is but it can either increase or decrease the tank's max speed");
+            tankCreator.ArmorType decision3 = javelin.armorTypeDecider();
+            if (decision3 == tankCreator.ArmorType.EXPLOSIVE_REACTIVE) {
+                System.out.println("Selected Armor Type: Explosive Reactive\n");
+                speedoption -= 4;
+                Thread.sleep(2000);
+            } else if (decision3 == tankCreator.ArmorType.NON_EXPLOSIVE_REACTIVE) {
+                System.out.println("Selected Armor Type: Non Explosive Reactive");
+                speedoption -= 2;
+                Thread.sleep(2000);
+            } else if (decision3 == tankCreator.ArmorType.COMPOSITE) {
+                System.out.println("Selected Armor Type: Composite");
+                Thread.sleep(2000);
+            } else if(decision3 == tankCreator.ArmorType.STEEL) {
+                System.out.println("Selected Armor Type: Steel");
+                speedoption -= 10;
+                Thread.sleep(2000);
+            }
+            
+            javelin.setArmorType(decision3);
+
+            System.out.println("Now its time to create your armor thickness. With the addition of your armor. For every 25mm you add your speed will decrease by atleast 1 so be cautios as your speed is set at " + javelin.getTankSpeed()1);
         } else if (decision == tankCreator.TankType.MEDIUM) {
             System.out.println("Selected Tank Type: Medium");
+        } else if (decision == tankCreator.TankType.HEAVY) {
+            System.out.println("Selected Tank Type: Heavy");
         }
     }
 }
